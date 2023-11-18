dc1ef859650eee2d78412254de8cdd3f85927145
BlackDP <blackdp@ya.ru>
2023-11-19 00:25:25 +0300

    L2_b_done

--- level_2/b_user_should_be_banned.py
+++ level_2/b_user_should_be_banned.py
@@ -11,4 +11,22 @@ SURNAMES_TO_BAN = ['Vaughn', 'Wilhelm', 'Santaros', 'Porter', 'Smith']
 
 
 class User:
-    pass  # код писать тут
+    def __init__(self, name: str, surname: str, age: int):
+        self.name = name
+        self.surname = surname
+        self.age = age
+    
+    def should_be_banned(self):
+        if self.surname in SURNAMES_TO_BAN:
+            return f'Пользователь забанен'
+            # print('Пользователь забанен')
+        return f'Все в порядке'
+        
+if __name__ == '__main__':
+    user1 = User(
+        name='Mike',
+        surname='Wilhelm',
+        age=35
+    )
+    check_user = user1.should_be_banned()
+    print(check_user)
