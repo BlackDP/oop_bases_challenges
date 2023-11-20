0f17bd529bad7149c8464852950a40d8b694286d
BlackDP <blackdp@ya.ru>
2023-11-20 22:51:40 +0300

    L3_a_done

--- level_3/a_credit_bank_account.py
+++ level_3/a_credit_bank_account.py
@@ -8,24 +8,43 @@
     4. Создать экземпляр класс CreditAccount и вызвать у него каждый из возможных методов.
 """
 
-# код писать тут
-
-
-class CreditAccount:
+class BankAccount:
     def __init__(self, owner_full_name: str, balance: float):
         self.owner_full_name = owner_full_name
         self.balance = balance
-
+        
     def increase_balance(self, amount: float):
         self.balance += amount
 
     def decrease_balance(self, amount: float):
         self.balance -= amount
 
+class CreditAccount(BankAccount):
     def is_eligible_for_credit(self):
         return self.balance > 1000
 
 
 if __name__ == '__main__':
-    pass  # код писать тут
-
+    bank_account = BankAccount(
+        owner_full_name='Feel Parrot',
+        balance=2000
+    )
+    print(f'Владелец счета: {bank_account.owner_full_name}, баланс:{bank_account.balance}')
+    bank_account.increase_balance(1000)
+    print(f'Поздравляем, счет пополнен, ваш баланс:{bank_account.balance}')
+    bank_account.decrease_balance(1500)
+    print(f'Расход со счета, ваш баланс:{bank_account.balance}')
+    credit_account = CreditAccount(
+        owner_full_name='Db. Swift',
+        balance=5000
+    )
+    print(f'Владелец счета: {credit_account.owner_full_name}, баланс:{credit_account.balance}')
+    credit_account.increase_balance(8000)
+    print(f'Поздравляем, счет пополнен, ваш баланс:{credit_account.balance}')
+    credit_account.decrease_balance(13000)
+    print(f'Расход со счета, ваш баланс:{credit_account.balance}')
+    check_credit = credit_account.is_eligible_for_credit()
+    if check_credit:
+        print('Вам доступно персональное предложение по кредитованию')
+    else:
+        print('Спасибо что обратились к нам, но к сожалению, клиент вы так себе...')
